34f72a16074c6ac271b07d0c24824f6b21cab4c8
Ahmad Hafizh Alkarim <hafizh.alkarim02@gmail.com>
2021-11-18 22:15:52 +0700

    Done problem 5

--- Assets/Scripts/InputMovement.cs
+++ Assets/Scripts/InputMovement.cs
@@ -6,9 +6,63 @@ public class InputMovement : MonoBehaviour
 {
     public float speed = 10f;
     public Rigidbody2D rb;
+    public InputType inputType;
 
+    public Camera mainCamera;
+
+    private Vector3 direction;
+    private Vector3 destination;
+    private bool startMove = false;
+
+    private void Start()
+    {
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+    }
     // Update is called once per frame
     void Update()
+    {
+        if (inputType == InputType.Keyboard)
+        {
+            MoveInputKeyboard();
+        }
+        else if (inputType == InputType.Mouse)
+        {
+            ReadInputMouse();
+            MoveToDestination();
+        }
+    }
+
+    private void ReadInputMouse()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var clickPos = mainCamera.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+            Debug.Log(clickPos);
+
+            destination = new Vector3(clickPos.x, clickPos.y);
+            direction = (destination - transform.position).normalized;
+
+            startMove = true;
+        }
+    }
+
+    private void MoveToDestination()
+    {
+        if (startMove)
+        {
+            rb.velocity = direction.normalized * speed * Time.fixedDeltaTime;
+
+            float remainingDist = Vector3.Distance(destination, transform.position);
+            if (remainingDist <= .1f)
+            {
+                startMove = false;
+                rb.velocity = Vector2.zero;
+            }
+        }
+    }
+
+    private void MoveInputKeyboard()
     {
         var h = Input.GetAxis("Horizontal");
         var v = Input.GetAxis("Vertical");
@@ -17,3 +71,5 @@ public class InputMovement : MonoBehaviour
         rb.velocity = dir.normalized * speed * Time.fixedDeltaTime;
     }
 }
+
+public enum InputType { Mouse, Keyboard }
